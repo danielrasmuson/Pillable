51d89ba567263c06d2bd3722f395bfeee638901b
Daniel Rasmuson <dan123911@gmail.com>
2014-10-26 10:15:35 -0500

    its now waiting for the pill before it loads

--- www/js/PillCtrl.js
+++ www/js/PillCtrl.js
@@ -6,32 +6,33 @@ angular.module('starter.controllers')
   var pathToPayLayoutExample = 'database/PillData.json';
   $.getJSON(pathToPayLayoutExample,function(result){
     $scope.pill = result[nameOfMedication];
+    $scope.getAverage = function(key){
+      total = 0;
+      count = 0;
+      for (var i = 0; i < $scope.pill.reviews.length; i++) {
+        total += $scope.pill.reviews[i][key];
+        count += 1;
+      }
+      return Math.round(total/count);
+    };
+
+    // todo put this in servce
+    $scope.writeReview = function(pillName){
+      window.location.replace("#/app/review/"+pillName);
+
+    };
+
+    setTimeout(function() {
+      $(document).ready(function() {
+          $('.progress .progress-bar').progressbar();
+      }); 
+    }, 20);
+
   });
 
   $scope.browser = function(link){
     var ref = window.open(link, '_blank', 'location=yes');
   };
 
-  $scope.getAverage = function(key){
-    total = 0;
-    count = 0;
-    for (var i = 0; i < $scope.pill.reviews.length; i++) {
-      total += $scope.pill.reviews[i][key];
-      count += 1;
-    }
-    return Math.round(total/count);
-  };
-
-  // todo put this in servce
-  $scope.writeReview = function(pillName){
-    window.location.replace("#/app/review/"+pillName);
-
-  };
-
-  setTimeout(function() {
-    $(document).ready(function() {
-        $('.progress .progress-bar').progressbar();
-    }); 
-  }, 20);
-
+  
 });
