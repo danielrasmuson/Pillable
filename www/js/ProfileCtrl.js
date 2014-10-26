d609c0cac8e9266b46504d780f3c26814cb6a42b
Daniel Rasmuson <dan123911@gmail.com>
2014-10-26 13:20:25 -0500

    about to remove wallgreens stuff

--- www/js/ProfileCtrl.js
+++ www/js/ProfileCtrl.js
@@ -64,15 +64,55 @@ angular.module('starter.controllers')
       );
     };
 
-   
+    // valid wallgreens data
+    // {
+    //     "creates": [
+    //         {
+    //             "access_token": "90c8b55aaafe542a9dd5331a1b9ce7f6a65b74f93a98261778188dd9519f8b18",
+    //             "affiliate_id": "pllpdse",
+    //             "date": "2014-05-05",
+    //             "device_name": "Pillable",
+    //             "manufacturer_name": "Pillable",
+    //             "transaction_id": 123,
+    //             "user_device_id": "9167332",
+    //             "data": [
+    //                 {
+    //                     "id": "a6e3502053df0131569d1231392",
+    //                     "timestamp": "2014-03-18 02:10:10",
+    //                     "type": "blood_pressure",
+    //                     "value": {
+    //                         "diastolic": "120",
+    //                         "systolic": "120"
+    //                     },
+    //                     "device_tracked": "true"
+    //                 },
+    //                 {
+    //                     "id": "a6e3502053df0131569d1231392",
+    //                     "timestamp": "2014-03-18 02:10:10",
+    //                     "type": "blood_oxygen_ratio",
+    //                     "value": "100",
+    //                     "device_tracked": "true"
+    //                 },
+    //                 {
+    //                     "id": "a6e3502053df0131569d1231392",
+    //                     "timestamp": "2014-03-18 02:10:10",
+    //                     "type": "weight",
+    //                     "value": "180",
+    //                     "device_tracked": "true"
+    //                 }
+    //             ]
+    //         }
+    //     ]
+    // } 
 
     $scope.syncWalgreen = function(){
       var apiKey = "Eu9ZiReoEQbKZu6GQzrJTLW2LsQiKJHk";
-      var urlToPost = "https://services-qa.walgreens.com/api/steps/activity/v1?apiKey="+apiKey;
+      // var urlToPost = "https://services-qa.walgreens.com/api/steps/activity/v1?apiKey="+apiKey;
+      var urlToPost = "http://localhost:3000/updateWalgreensMetrics";
       var token = "90c8b55aaafe542a9dd5331a1b9ce7f6a65b74f93a98261778188dd9519f8b18";
       var transaction_id = "904986923560504";
 
-      var dataToPost =  {"creates": [{"access_token": token, "affiliate_id": "pllpdse", "data": [{"device_tracked": "true", "id": "a6e3502053df0131569d1231392xxxxx", "timestamp": "2014-03-18 02:10:10", "type": "blood_pressure", "value": {"diastolic": $scope.bloodPressure, "systolic": $scope.bloodPressure } }, {"device_tracked": "true", "id": "a6e3502053df0131569d1231392xxxxx", "timestamp": "2014-03-18 02:10:10", "type": "blood_oxygen_ratio", "value": $scope.glucose }, {"device_tracked": "true", "id": "a6e3502053df0131569d1231392xxxxx", "timestamp": "2014-03-18 02:10:10", "type": "weight", "value": $scope.weight } ], "date": "2014-05-05", "device_name": "Pillable", "manufacturer_name": "Pillable", "transaction_id": transaction_id, "user_device_id": "9167332"} ] };
+      var dataToPost =  {"creates": [{"access_token": token, "affiliate_id": "pllpdse", "date": "2014-05-05", "device_name": "Pillable", "manufacturer_name": "Pillable", "transaction_id": transaction_id, "user_device_id": "9167332", "data": [{"device_tracked": "true", "id": "a6e3502053df0131569d1231392", "timestamp": "2014-03-18 02:10:10", "type": "blood_pressure", "value": {"diastolic": $scope.bloodPressure, "systolic": $scope.bloodPressure } }, {"device_tracked": "true", "id": "a6e3502053df0131569d1231392", "timestamp": "2014-03-18 02:10:10", "type": "blood_oxygen_ratio", "value": $scope.glucose }, {"device_tracked": "true", "id": "a6e3502053df0131569d1231392", "timestamp": "2014-03-18 02:10:10", "type": "weight", "value": $scope.weight } ] } ] };
 
       $http.post(urlToPost, dataToPost)
       .then(function (result) {
