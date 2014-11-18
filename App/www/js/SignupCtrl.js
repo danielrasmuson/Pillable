09082908374cfd0c629717814d6ee5c349e50173
Daniel Rasmuson <dan123911@gmail.com>
2014-11-17 20:52:19 -0600

    added binding to signup

--- App/www/js/SignupCtrl.js
+++ App/www/js/SignupCtrl.js
@@ -1,5 +1,11 @@
 angular.module('starter.controllers')
 // todo put this location stuff in service
 .controller('SignupCtrl', function($scope) {
+    $scope.userData = {
+        firstName: "", 
+        lastName: "", 
+        email: "", 
+        password: ""
+    }
 
 });
