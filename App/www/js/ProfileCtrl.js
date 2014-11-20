1195096a591d17be4298eca1d69c748e6de68e34
Daniel Rasmuson <dan123911@gmail.com>
2014-11-19 19:12:13 -0600

    added a run sync to profile section

--- App/www/js/ProfileCtrl.js
+++ App/www/js/ProfileCtrl.js
@@ -70,11 +70,63 @@ angular.module('starter.controllers')
     $scope.syncWalgreen = function(){
         $scope.loading = true;
         $scope.walgreensText = "Syncing";
+        runSync();
         setTimeout(function() {
             $scope.loading = false;
             $scope.successSync = true;
             $scope.walgreensText = "Sync Successful!";
             $scope.$apply();
         }, 1500);
+    };
+
+    function runSync(token, transaction_id, bloodPressure, glucose, weight){
+      var apiKey = "Eu9ZiReoEQbKZu6GQzrJTLW2LsQiKJHk";
+      // var urlToPost = "https://services-qa.walgreens.com/api/steps/activity/v1?apiKey="+apiKey;
+      var urlToPost = "http://localhost:3000/updateWalgreensMetrics";
+      var token = "90c8b55aaafe542a9dd5331a1b9ce7f6a65b74f93a98261778188dd9519f8b18";
+      var transaction_id = "904986923560504";
+
+      var dataToPost =  {"creates": [
+        {
+          "access_token": token,
+          "affiliate_id": "pllpdse",
+          "date": "2014-05-05",
+          "device_name": "Pillable",
+          "manufacturer_name": "Pillable",
+          "transaction_id": transaction_id,
+          "user_device_id": "9167332",
+          "data": [
+            {
+              "device_tracked": "true",
+              "id": "a6e3502053df0131569d1231392",
+              "timestamp": "2014-03-18 02:10:10",
+              "type": "blood_pressure",
+              "value": {
+                "diastolic": bloodPressure,
+                "systolic": bloodPressure 
+              } 
+            },
+            {
+              "device_tracked": "true",
+              "id": "a6e3502053df0131569d1231392",
+              "timestamp": "2014-03-18 02:10:10",
+              "type": "blood_oxygen_ratio",
+              "value": glucose 
+            },
+            {
+              "device_tracked": "true",
+              "id": "a6e3502053df0131569d1231392",
+              "timestamp": "2014-03-18 02:10:10",
+              "type": "weight",
+              "value": weight 
+            } 
+          ]
+        }
+      ]};
+
+      $http.post(urlToPost, dataToPost)
+      .then(function (result) {
+         alert(result.data);
+      });
     };
 });
