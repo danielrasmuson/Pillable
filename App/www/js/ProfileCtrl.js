b5702e8489b46e9ddd96bea58c97c2d0fa7b07b5
Daniel Rasmuson <dan123911@gmail.com>
2014-11-24 16:46:44 -0600

    got a successful user health add from app page

--- App/www/js/ProfileCtrl.js
+++ App/www/js/ProfileCtrl.js
@@ -1,5 +1,5 @@
 angular.module('starter.controllers')
-.controller('ProfileCtrl', function($scope, $location,  $ionicPopup, $http) {
+.controller('ProfileCtrl', function($scope, $location,  $ionicPopup, $http, UserService, UrlService) {
     $scope.profile = {
       name:  "Daniel Rasmuson",
       weight:  155,
@@ -71,13 +71,19 @@ angular.module('starter.controllers')
         $scope.loading = true;
         $scope.walgreensText = "Syncing";
 
-        // todo this pulls from database
-        var token = "90c8b55aaafe542a9dd5331a1b9ce7f6a65b74f93a98261778188dd9519f8b18";
-        // todo this is generated randomly
-        var transaction_id = "904986923560504";
+        var healthData = {
+          weight: $scope.profile.weight,
+          bloodPressure: $scope.profile.bloodPressure,
+          glucose: $scope.profile.glucose,
+          session: UserService.getSession().session
+        }; 
 
-
-        runSync(token, transaction_id, $scope.profile.bloodPressure, $scope.profile.glucose, $scope.profile.weight);
+        console.log(UrlService.baseURL+'/add/health');
+        console.log(healthData);
+        $http.post(UrlService.baseURL+'/add/health', healthData)
+        .then(function (result) {
+          console.log(result.data);
+        });
 
         setTimeout(function() {
             $scope.loading = false;
@@ -87,57 +93,4 @@ angular.module('starter.controllers')
         }, 1500);
     };
 
-    function runSync(token, transaction_id, bloodPressure, glucose, weight){
-
-      var apiKey = "Eu9ZiReoEQbKZu6GQzrJTLW2LsQiKJHk";
-      // var urlToPost = "https://services-qa.walgreens.com/api/steps/activity/v1?apiKey="+apiKey;
-
-      var dataToPost =  {
-        "creates": [
-          {
-            "access_token": token,
-            "affiliate_id": "pllpdse",
-            "date": "2014-05-05",
-            "device_name": "Pillable",
-            "manufacturer_name": "Pillable",
-            "transaction_id": transaction_id,
-            "user_device_id": "9167332",
-            "data": [
-              {
-                "device_tracked": "true",
-                "id": "a6e3502053df0131569d1231392",
-                "timestamp": "2014-03-18 02:10:10",
-                "type": "blood_pressure",
-                "value": {
-                  "diastolic": bloodPressure,
-                  "systolic": bloodPressure 
-                } 
-              },
-              {
-                "device_tracked": "true",
-                "id": "a6e3502053df0131569d1231392",
-                "timestamp": "2014-03-18 02:10:10",
-                "type": "blood_oxygen_ratio",
-                "value": glucose 
-              },
-              {
-                "device_tracked": "true",
-                "id": "a6e3502053df0131569d1231392",
-                "timestamp": "2014-03-18 02:10:10",
-                "type": "weight",
-                "value": weight 
-              } 
-            ]
-          }
-        ]
-      };
-
-      var urlToPost = "http://localhost:3000/updateWalgreensMetrics";
-      console.log(urlToPost);
-      console.log(dataToPost);
-      // $http.post(urlToPost, dataToPost)
-      // .then(function (result) {
-      //    alert(result.data);
-      // });
-    };
 });
