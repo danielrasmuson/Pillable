71d4cb928ac715ec4bb0c5651af99ac622ed0e6f
Daniel Rasmuson <dan123911@gmail.com>
2014-11-29 19:13:17 -0600

    updated test to reflect expected value

--- test/getSession.js
+++ test/getSession.js
@@ -1,11 +1,19 @@
 var assert = require("assert");
 var getSession = require("../scripts/getSession").getSession;
+var expect = require('chai').expect;
 
 var ACTIVE_USERID = 4;
 
 describe('Get Session', function(){
+    var session;
+    before(function(done){
+        getSession(ACTIVE_USERID).then(function(userSession){
+            session = userSession;
+            done();
+        });
+    });
+
     it('should get valid session', function(){
-        console.log(JSON.stringify(getSession(ACTIVE_USERID)));
-        assert.equal("number", typeof getSession(ACTIVE_USERID));
+        expect(session).to.be.a('string') 
     });
 })
