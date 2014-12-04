8dde2183c7ce0f9a9fe53841acfdaaf6ee014c58
Daniel Rasmuson <dan123911@gmail.com>
2014-12-03 19:27:22 -0600

    added a decent story page

--- App/www/js/StoryCtrl.js
+++ App/www/js/StoryCtrl.js
@@ -2,7 +2,14 @@ angular.module('starter.controllers')
 .controller('StoryCtrl', function($scope, $location, $http, UrlService) {
     $scope.reviewNum = $location.path().split('/').pop();
 
+    $scope.storyData = {};
     $http.get(UrlService.baseURL+'/story/'+$scope.reviewNum).then(function (response) {
         console.log(response.data);
+        $scope.storyData = response.data;
+        setTimeout(function() {
+          $(document).ready(function() {
+              $('.progress .progress-bar').progressbar();
+          }); 
+        }, 50);
     }); 
 });
